9ae1a665571ba8d4a2b436e69e63ce06f57ed087
RanolP <ranolp@laftel.net>
2023-03-27 10:54:40 +0900

    feat: simple 'Go Random!' button in HomeScreen

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,11 +1,16 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React, {ReactNode} from 'react';
-import OldApp from './screens/OldApp';
+import {HomeScreen} from './screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
 
 export function Root(): ReactNode {
   return (
     <NavigationContainer>
-      <OldApp />
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,5 +1,10 @@
-import React, {ReactNode} from 'react';
+import React from 'react';
+import {Button, SafeAreaView} from 'react-native';
 
-export function HomeScreen(): ReactNode {
-  return null;
+export function HomeScreen() {
+  return (
+    <SafeAreaView>
+      <Button title="Go Random!" />
+    </SafeAreaView>
+  );
 }
