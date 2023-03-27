1a5b4024407e50c50af7ceac57ebac66698ebc6b
RanolP <ranolp@laftel.net>
2023-03-27 10:56:11 +0900

    chore: follow laftel-global-app naming rule

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,7 @@
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React, {ReactNode} from 'react';
-import {HomeScreen} from './screens/HomeScreen';
+import {HomeScreen} from './screens/home';
 
 const Stack = createNativeStackNavigator();
 
