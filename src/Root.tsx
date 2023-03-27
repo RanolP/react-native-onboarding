ebeb3752e7b1fd86252547dc1a9b9cd7479c4b75
RanolP <ranolp@laftel.net>
2023-03-27 10:38:01 +0900

    feat: initial navigation setup

---
+++ src/Root.tsx
@@ -0,0 +1,6 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React, {ReactNode} from 'react';
+
+export function Root(): ReactNode {
+  return <NavigationContainer>test</NavigationContainer>;
+}
---
+++ src/screens/HomeScreen/index.tsx
@@ -0,0 +1,5 @@
+import React, {ReactNode} from 'react';
+
+export function HomeScreen(): ReactNode {
+  return null;
+}
